d10aabcd46c3c8a6d28f474581067e583cc6f00b
Tom Wambold <tom5760@gmail.com>
2009-07-18 18:49:40 -0400

    Added Volume widget to its own plugin.

---
+++ plugins/volume.py
@@ -0,0 +1,39 @@
+from alsaaudio import Mixer, ALSAAudioError
+
+from wmii import Widget
+
+class Volume(Widget):
+    def __init__(self, name, voldev='Master', mutedev='Master', step=2):
+        super(Volume, self).__init__(name, 'rbar')
+        self.voldev = voldev
+        self.mutedev = mutedev
+        self.step = step
+        self.update()
+
+    def update(self):
+        if bool(Mixer(self.mutedev).getmute()[0]):
+            self.label = 'Vol: Mute'
+        else:
+            self.label = 'Vol: {0}%'.format(Mixer(self.voldev).getvolume()[0])
+        return None
+
+    def click(self, button):
+        if button == 1:
+            mixer = Mixer(self.mutedev)
+            mixer.setmute(not bool(mixer.getmute()[0]))
+
+        elif button == 4:
+            mixer = Mixer(self.voldev)
+            try:
+                mixer.setvolume(mixer.getvolume()[0] + self.step)
+            except ALSAAudioError:
+                return
+
+        elif button == 5:
+            mixer = Mixer(self.voldev)
+            try:
+                mixer.setvolume(mixer.getvolume()[0] - self.step)
+            except ALSAAudioError:
+                return
+
+        self.update()
